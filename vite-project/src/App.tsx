ce41712fc9c40a5e4ed91549e9ad79e636a7bf64
Indika <45280177+IndiW@users.noreply.github.com>
2023-09-21 21:30:31 -0400

    Add initial types for letters

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,90 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type AlphabetLetter =
+    | 'A'
+    | 'B'
+    | 'C'
+    | 'D'
+    | 'E'
+    | 'F'
+    | 'G'
+    | 'H'
+    | 'I'
+    | 'J'
+    | 'K'
+    | 'L'
+    | 'M'
+    | 'N'
+    | 'O'
+    | 'P'
+    | 'Q'
+    | 'R'
+    | 'S'
+    | 'T'
+    | 'U'
+    | 'V'
+    | 'W'
+    | 'X'
+    | 'Y'
+    | 'Z'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const alphabetArray: AlphabetLetter[] = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+]
+
+const MAX_HAND_SIZE = 7
+
+function App() {
+    const [letterCount, setLetterCount] = useState(20)
+    const [currentLetters, setCurrentLetters] = useState<string[]>([])
+
+    const getRandomValue = (arr: Array<string>) => {
+        const randomIndex = Math.floor(Math.random() * arr.length)
+        return arr[randomIndex]
+    }
+
+    const generateLetterSet = () => {
+        let currentLetterSetLength = currentLetters.length
+        const newArray = currentLetters
+        while (currentLetterSetLength <= MAX_HAND_SIZE) {
+            newArray.push(getRandomValue(alphabetArray))
+            currentLetterSetLength += 1
+        }
+        setCurrentLetters(newArray)
+    }
+
+    return (
+        <>
+            <h1>{getRandomValue(alphabetArray)}</h1>
+            <h1>{`Remaining letters: ${letterCount}`}</h1>
+        </>
+    )
 }
 
 export default App
